985fe6f7b5d00069653f35214e468a1074b86015
ginnocenitni <130387251+ginnocenitni@users.noreply.github.com>
2023-04-10 15:41:11 -0300

    Update streamlit_app.py
    Changing index os pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ import pandas as pd
 
 path1 = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 my_fruit_list = pd.read_csv(path1)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
