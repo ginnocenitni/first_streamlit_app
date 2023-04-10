6828d44e8f23f98e65f4aaf4ea6e39a781bfb672
ginnocenitni <130387251+ginnocenitni@users.noreply.github.com>
2023-04-10 15:51:05 -0300

    Update streamlit_app.py
    Using loc for selected fruits to be shown.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,10 @@ path1 = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 my_fruit_list = pd.read_csv(path1)
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index) , ['Avocado' , 'Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include
+# also, we will use a list to .loc table and exhibit just selected fruits
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index) , ['Avocado' , 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
