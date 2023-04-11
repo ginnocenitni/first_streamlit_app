b5d2a8c491b577d1c01ab74c456da8a05cb25bd9
ginnocenitni <130387251+ginnocenitni@users.noreply.github.com>
2023-04-11 17:05:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,8 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+streamlit.stop()
+
 # Let's put a pick list here so they can ADD the fruit they want to include
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding:', add_my_fruit)
